2ad1d8445d4fac9778cfc960b21af7f567f3825c
José Sánchez Díez <jsanchezd@Itteria.com>
2019-02-15 12:47:59 +0100

    Router para encuesta y mapa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,26 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login.component';
 import { MensajesComponent } from './pages/mensajes/mensajes.component';
+import { GraficaComponent } from './components/grafica/grafica.component';
 import { UsuarioGuard } from './guards/usuario-guard.service';
+import { MapaComponent } from './components/mapa/mapa.component';
+
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   {
     path: 'mensajes',
     component: MensajesComponent,
+    canActivate: [ UsuarioGuard ]
+  },
+  {
+    path: 'encuesta',
+    component: GraficaComponent,
+    canActivate: [ UsuarioGuard ]
+  },
+  {
+    path: 'mapa',
+    component: MapaComponent,
     canActivate: [ UsuarioGuard ]
   },
   { path: '**', component: LoginComponent }
